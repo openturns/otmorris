205760ace87bda5ae2764309ffffac38190d066b
Julien Schueller <schueller@phimeca.com>
2024-11-04 17:18:00 +0100

    Fix examples

--- python/doc/examples/plot_morris_grid.py
+++ python/doc/examples/plot_morris_grid.py
@@ -12,13 +12,16 @@ import openturns as ot
 import openturns.viewer as otv
 import otmorris
 
+# %%
 # use the reference 20-d function from the Morris paper
 f = ot.Function(otmorris.MorrisFunction())
 dim = f.getInputDimension()
 
+# %%
 # Number of trajectories
 r = 10
 
+# %%
 # Define experiments in [0,1]^20
 # p-levels
 p = 5
@@ -27,13 +30,16 @@ bounds = ot.Interval(dim)  # [0,1]^d
 X = morris_experiment.generate()
 Y = f(X)
 
+# %%
 # Evaluate Elementary effects (ee)
 morris = otmorris.Morris(X, Y, bounds)
 
+# %%
 # Compute mu/sigma
 mean = morris.getMeanAbsoluteElementaryEffects()
 sigma = morris.getStandardDeviationElementaryEffects()
 graph = morris.drawElementaryEffects(0)
 view = otv.View(graph)
 
+# %%
 otv.View.ShowAll()
--- python/doc/examples/plot_morris_lhs.py
+++ python/doc/examples/plot_morris_lhs.py
@@ -4,7 +4,7 @@ Example 2: Morris use-case and LHS initial design
 """
 
 # %%
-# To define the trajectories, we first get an LHS design in the box :math:`[0,1]^{20}` of :math:`size=50`
+# To define the trajectories, we first get an LHS design in the box :math:`[0,1]^{20}` of size :math:`50`
 # We set the number of trajectories input variables are randomly to 10.
 
 # %%
@@ -12,13 +12,16 @@ import openturns as ot
 import otmorris
 import openturns.viewer as otv
 
+# %%
 # use the reference 20-d function from the Morris paper
 f = ot.Function(otmorris.MorrisFunction())
 dim = f.getInputDimension()
 
+# %%
 # Number of trajectories
 r = 10
 
+# %%
 # Define an LHS experiment of size 50 in [0, 1]^20
 size = 50
 dist = ot.ComposedDistribution([ot.Uniform(0, 1)] * dim)
@@ -29,9 +32,11 @@ bounds = ot.Interval(dim)  # [0, 1]^20
 X = morris_experiment.generate()
 Y = f(X)
 
+# %%
 # Evaluate Elementary effects (ee)
 morris = otmorris.Morris(X, Y, bounds)
 
+# %%
 # Compute mu/sigma
 mean = morris.getMeanAbsoluteElementaryEffects()
 sigma = morris.getStandardDeviationElementaryEffects()
@@ -39,4 +44,5 @@ graph = morris.drawElementaryEffects(0)
 graph.setTitle("Elementary Effects using LHS")
 view = otv.View(graph)
 
+# %%
 otv.View.ShowAll()

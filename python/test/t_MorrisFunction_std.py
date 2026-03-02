baa0e99a9472aeb639fdd4bc80616f62888470f7
Michael Baudin <michael.baudin@gmail.com>
2026-03-02 22:59:19 +0100

    python/test/t_MorrisFunction_std.py: Fixed

--- python/test/t_MorrisFunction_std.py
+++ python/test/t_MorrisFunction_std.py
@@ -6,7 +6,11 @@ import otmorris
 import time
 import numpy as np
 
-g = ot.Function(otmorris.MorrisFunction())
+ot.RandomGenerator.SetSeed(1)
+b0_random = ot.DistFunc.rNormal()
+b1_random = ot.DistFunc.rNormal(10)
+b2_random = ot.DistFunc.rNormal(175)
+g = ot.Function(otmorris.MorrisFunction(b0_random, b1_random, b2_random))
 dim = g.getInputDimension()
 
 # Check accuracy
